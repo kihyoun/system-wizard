df34e72df8f64b2243c5b09531da30b37987aeb4
Ki-Hyoun <kihyoun@gmail.com>
2020-11-13 12:34:34 +0100

    masquerade

--- src/api/ProjectConfig.tsx
+++ src/api/ProjectConfig.tsx
@@ -224,44 +224,44 @@ export default class ProjectConfig implements ProjectConfigInterface {
    public get content() : string {
      const ret = Helper.textLogo + `# Filepath: ./.projects.env/.${this.projectKey}.env
 
-USE_PROD_HOST=${this.useProdHost}
+USE_PROD_HOST='${this.useProdHost}'
 `;
      const prod = `
-PROJECT_NAME=${this.projectKey}
-PROD_HOST=${this.prodHost}
+PROJECT_NAME='${this.projectKey}'
+PROD_HOST='${this.prodHost}'
 PROD_DOMAIN_MODE=${this.prodDomainMode}
 `;
      const prodSSL = `
-PROD_SSL=${this.prodSSL}
-PROD_SSL_KEY=${this.prodSSLKey}
+PROD_SSL='${this.prodSSL}'
+PROD_SSL_KEY='${this.prodSSLKey}'
 `;
      const betaUse = `
 # Set false if not using a Beta Deployment
-USE_BETA_HOST=${this.useBetaHost}
+USE_BETA_HOST='${this.useBetaHost}'
 `;
      const beta = `
-BETA_HOST=${this.betaHost}
+BETA_HOST='${this.betaHost}'
 BETA_DOMAIN_MODE=${this.betaDomainMode}
 `;
      const betaSSL = `
-BETA_SSL=${this.betaSSL}
-BETA_SSL_KEY=${this.betaSSLKey}
+BETA_SSL='${this.betaSSL}'
+BETA_SSL_KEY='${this.betaSSLKey}'
 `;
      const reviewUse = `
 # Set false if not using the Review
 USE_REVIEW_HOST=${this.useReviewHost}
 `;
      const reviewHost =`
-REVIEW_HOST=${this.reviewHost}
+REVIEW_HOST='${this.reviewHost}'
 REVIEW_DOMAIN_MODE=${this.reviewDomainMode}
 `;
      const reviewSSL = `
-REVIEW_SSL=${this.reviewSSL}
-REVIEW_SSL_KEY=${this.reviewSSLKey}
+REVIEW_SSL='${this.reviewSSL}'
+REVIEW_SSL_KEY='${this.reviewSSLKey}'
   `;
      const gitlabRunner = `
 # -- GITLAB RUNNER for Project
-GITLAB_RUNNER_TOKEN=${this.gitlabRunnerToken}
+GITLAB_RUNNER_TOKEN='${this.gitlabRunnerToken}'
 export GITLAB_RUNNER_SCALE=${this.gitlabRunnerScale}
 `;
      const createdOn = `# created on ${new Date()}`;
