009e45e0e1f1558193662254277f35d5c6fba23b
Ki-Hyoun <kihyoun@gmail.com>
2020-11-14 20:02:00 +0100

    fix: missing source

--- stop.sh
+++ stop.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
 if [ -f ../system/.docker.env ]; then
+    source ../system/.docker.env
+
     if [ $WIZARD_ENABLE = true ]; then
         docker-compose -f ../system/docker-compose.yml stop wizard
     fi
