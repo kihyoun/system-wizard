907f0f84783d3e11921dc5eee8c73ff28cc5564b
Ki-Hyoun <kihyoun@gmail.com>
2020-11-14 18:36:48 +0100

    fix: add missing wizard ip

--- start.sh
+++ start.sh
@@ -8,3 +8,6 @@ else
     docker-compose -f ../system/docker-compose.yml up --remove-orphans --build -d wizard
 fi
 
+echo > .docker.env
+printf "WIZARD_IP=" > .docker.env
+docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' system_wizard_1 >> .docker.env
