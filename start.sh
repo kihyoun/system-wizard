485f6a29a3e78a26e5af566a1ef504d67ab6ed33
Ki-Hyoun <kihyoun@gmail.com>
2020-11-14 20:57:47 +0100

    fix: start failure due to missing source

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #l! /bin/bash
 
 if [ -f ../system/.docker.env ]; then
+    source ../system/.docker.env
+
     if [ $WIZARD_ENABLE = true ]; then
         docker-compose -f ../system/docker-compose.yml up --remove-orphans --build -d wizard
     fi
