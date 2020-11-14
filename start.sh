03c18da527724e6fd2e44e96138621a534768590
Ki-Hyoun <kihyoun@gmail.com>
2020-11-14 15:49:19 +0100

    update start script

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #l! /bin/bash
-if [ -f ../system/docker-compose.yml ]; then
+if [ -f ../system/docker-compose.yml ] && [ -f ../system/.docker.env ]; then
     docker-compose -f ../system/docker-compose.yml up --remove-orphans --build -d wizard
+    echo > .docker.env
+    printf "WIZARD_IP=" > .docker.env
+    docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' system_wizard_1 >> .docker.env
 else
-    docker-compose -p system up --remove-orphans --build -d
+    echo "wizard [start] skipped (no main conf)"
 fi
 
-echo > .docker.env
-printf "WIZARD_IP=" > .docker.env
-docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' system_wizard_1 >> .docker.env
